47d945c91f7b1d9db8b32f14a386a5ace0aeab3a
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-03 21:20:49 -0300

    Aprendendo sobre variáveis

---
+++ JavaScript - Hora de Codar/3_particularidades_js/js/script.js
@@ -0,0 +1,18 @@
+console.log('Primeiro')
+
+
+console.log('Último')
+
+var a = 1  // <-- declara uma variavel
+
+
+
+// Tipos de comentário
+
+// -> usado para uma linha
+
+/* ->   usado 
+        para 
+        multiplas 
+        linhas  
+*/ 
---
+++ JavaScript - Hora de Codar/4_variaveis/js/script.js
@@ -0,0 +1,9 @@
+var teste = 1
+
+
+console.log(teste)
+
+
+let testando = 1 // Aprender as diferenças nas próximas aulas
+
+const ola = 2 // Aprender as diferenças nas próximas aulas
