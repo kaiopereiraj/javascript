4871cdeaf9d8aa803bde16d0c5b810b0ca927105
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-04 21:33:12 -0300

    Aprendendo sobre string

---
+++ JavaScript - Hora de Codar/6_string/js/script.js
@@ -0,0 +1,20 @@
+var nome = 'Kaio'
+var sobrenome = 'Pereira'
+
+
+
+var nomeCompleto = nome + ' ' + sobrenome
+
+console.log(nome)
+console.log(typeof nome)
+console.log(nomeCompleto)
+
+var frase = 'Uma frase simples'
+
+console.log(frase)
+console.log(typeof frase)
+
+console.log(nome + ' ' + frase)
+
+document.write('Hello, world!')
+
