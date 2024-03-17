514f66e7d999b1a04afd2307cd2551baa715d5ea
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-17 00:28:24 -0300

    Entendendo if - aula 11

---
+++ JavaScript - Hora de Codar/11_if/js/script.js
@@ -0,0 +1,17 @@
+var idade = 21
+var idadeMinima = 18
+
+console.log('Antes do if')
+
+//Operador condicional
+if (idade > 17) { //Se idade maior que 18, execute o console.log('Pode fazer a cnh')
+    console.log('Pode fazer a cnh')
+}
+
+/* 
+if (idade > idadeMinima) { 
+    console.log('Pode fazer a cnh')
+}
+*/
+
+console.log('Depois do if')
