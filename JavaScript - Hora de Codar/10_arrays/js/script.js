9eace34d7b566537ff7ac754986518ff5a243e57
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-17 00:17:16 -0300

    Entendendo arrays - aula 10

---
+++ JavaScript - Hora de Codar/10_arrays/js/script.js
@@ -0,0 +1,20 @@
+var arr = [21, 'Kaio', true, {teste: 1, test:2}] //obj -> {}
+
+console.log(arr)
+
+var arr2 = [1, 2, 3, 4, 5]
+
+console.log(arr2)
+
+console.log(arr[1])
+console.log(arr[0])
+
+arr[4] = 10
+
+console.log(arr)
+
+arr[0] = 'Testando'
+
+console.log(arr)
+
+console.log(typeof arr)
