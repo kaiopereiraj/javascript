0c2066fab95b8925aa3413ababc023cae6cada63
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-30 15:24:28 -0300

    Operadores de comparação - JS

---
+++ JavaScript - Hora de Codar/12_op_comparacao/js/script.js
@@ -0,0 +1,21 @@
+var idade = 18
+
+if(idade >= 18) {
+    console.log('O usuário pode fazer a cnh')
+}
+
+
+if(idade <= 17) {
+    console.log('O usuário não pode fazer a cnh')
+}
+
+var nome = 'Kaio'
+
+if (nome == 'Kaio'){
+    console.log(`O seu nome está dentro da variavel nome`)
+}
+
+if (nome != 'Kaio'){
+    console.log(`O nome não é Kaio`)
+}
+
