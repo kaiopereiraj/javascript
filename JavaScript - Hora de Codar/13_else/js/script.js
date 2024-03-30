8fd714e7e76bfc5398854420a79cf0d2bd2eb0f3
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-30 16:34:41 -0300

    Else if e Else - JS

---
+++ JavaScript - Hora de Codar/13_else/js/script.js
@@ -0,0 +1,25 @@
+var nome = 'Kaio'
+
+if (nome == 'Julia') {
+    console.log('O nome dele é Julia')
+} else if (nome == 'Kaio') {
+    console.log('O nome é Kaio')
+} else if (nome == 'Nana'){
+    console.log('O nome é Nana')
+} else {
+    console.log('Ele possui outro nome!')
+}
+
+var idade = 21
+
+if (idade > 22) {
+    console.log('Ele pode entrar na festa')
+} else if (idade >= 18){
+    console.log('Ele só pode entrar com autorização')
+}
+
+if (nome == 'Kaio') {
+    console.log('Teste')
+} else {
+    console.log('Testando')
+}
