de5d93e489cc1bd37f49fe5dff36c7df62519799
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-30 17:24:14 -0300

    Operador NOT - JS

---
+++ JavaScript - Hora de Codar/17_operador_NOT/js/script.js
@@ -0,0 +1,13 @@
+if (!true) {
+    console.log('Passou')
+} else {
+    console.log('False')
+}
+
+var nome = 'Kaio'
+
+if (!(nome == 'Kaio')) {
+    console.log('False')
+} else {
+    console.log('Ok')
+}
