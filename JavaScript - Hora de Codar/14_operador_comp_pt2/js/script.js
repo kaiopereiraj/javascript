aa533941fc1a9190e735c788d28838f0704d38d4
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-30 16:51:21 -0300

    Comparação de valor e tipo de dado - JS

---
+++ JavaScript - Hora de Codar/14_operador_comp_pt2/js/script.js
@@ -0,0 +1,11 @@
+var numero = '5'
+
+// Comparação de valor e tipo de dado
+
+if (numero === 5) {
+    console.log('O numero é 5')
+}
+
+if (numero !== 5) {
+    console.log('Não é o numero 5 do tipo Number')
+}
