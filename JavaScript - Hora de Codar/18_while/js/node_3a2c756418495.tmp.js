c9accc642c7a8a18105738f4c696be414353b9c7
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-30 22:14:20 -0300

    While - JS

---
+++ JavaScript - Hora de Codar/18_while/js/node_3a2c756418495.tmp.js
@@ -0,0 +1,15 @@
+var x = 0
+
+while(x < 5) {
+    console.log(`Testando repetição ${x}`)
+
+    // Incrementador
+    x++
+}
+
+var arr = ['Teste', 'Testando', 'a', 'b']
+var y = 0
+
+while(y <= 3){
+    console.log(arr[y])
+}
