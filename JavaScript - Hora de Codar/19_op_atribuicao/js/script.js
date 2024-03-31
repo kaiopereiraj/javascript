f1349093de8e8805186b8b1e505f270ba50b96e8
kaiopereiraj <155786048+kaiopereiraj@users.noreply.github.com>
2024-03-30 22:51:21 -0300

    Atribuição - JS

---
+++ JavaScript - Hora de Codar/19_op_atribuicao/js/script.js
@@ -0,0 +1,34 @@
+//Soma
+var x = 1
+var y = 2
+
+console.log(x += y)
+console.log(x = x + y)
+
+//Subtração
+
+var x = 8
+var y = 2
+
+console.log(x -= y)
+
+//Multiplicação
+
+console.log(x *= y)
+
+//Divisão
+
+console.log(x /= y)
+
+while(x < 100){
+    console.log(x)
+    x *= 2
+}
+
+console.log(x)
+
+var j = 14
+while(x > 0) {
+    console.log(x)
+    x -= j
+}
