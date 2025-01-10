0772ed2e8bfc947924ed652f00738d6c9c28878a
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-09 21:42:23 -0300

    v1.1

--- cursoemvideo/aula016ex/ex018/script.js
+++ cursoemvideo/aula016ex/ex018/script.js
@@ -21,8 +21,24 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        alert('Ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        alert('Adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
     }
 }
