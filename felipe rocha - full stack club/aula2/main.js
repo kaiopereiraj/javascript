d37ab2ea80e52ffcdb28e5e9f36c54042f856325
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-10 22:38:30 -0300

    aula02

---
+++ felipe rocha - full stack club/aula2/main.js
@@ -0,0 +1,61 @@
+/* 
+
+const --- Não é possivel alterar
+
+let --- É possivel alterar
+
+
+
+let  message = 'Hello World!'
+
+message = 'Olá Mundo!' - O que está dentro da variável está sendo alterado.
+
+alert(message)
+
+*/
+
+/* Strings
+
+const firstName = 'Kaio';
+const lastName = 'Jesus'
+
+const names = 'Kaio, Joaquim, Gil'
+
+console.log('Meu nome é ' + firstName + ' ' + lastName) //Concatenação
+
+console.log(`Meu nome é ${firstName} ${lastName}`) //Template Strings
+
+console.log(names.split(","))
+
+*/
+
+/* Number
+
+const number = 5;
+
+console.log(number);
+console.log(typeof number);
+
+*/
+
+/* Booleanos
+
+true  --- Verdadeiro
+
+false --- Falso
+
+
+console.log( 2 == 3) -> Resultará em false, pois 2 não é igual a 3
+*/
+
+//Null & Undefined
+
+const x = null;
+
+const y = undefined; 
+
+
+// Listas
+
+const list = [1, 2, 3. 4];
+const object = {name: 'Kaio'};
