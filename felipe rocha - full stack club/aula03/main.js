e613a4ba65bc83db6993480a35ca6c8b9da3a585
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-11 22:14:17 -0300

    Update main.js

---
+++ felipe rocha - full stack club/aula03/main.js
@@ -0,0 +1,16 @@
+const names =['Kaio', 'Joaquim', 'Gil', 100];
+
+const joaquim = names[1];
+
+console.log(joaquim);
+
+// names.push('Brasil'); //Vai adicionar mais um item no final da lista
+
+names.unshift('Ivonete'); // Vai adicionar mais um item no inicio da lista
+
+names.pop(); // Vai remover o último item da lista ...
+
+console.log(names.indexOf('Gil'));
+
+
+console.log(names);
