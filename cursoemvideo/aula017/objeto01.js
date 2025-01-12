d1878a7561d5de5223a52f0e29ca1ef1b7e110cd
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-12 15:47:44 -0300

    Create objeto01.js

---
+++ cursoemvideo/aula017/objeto01.js
@@ -0,0 +1,12 @@
+let amigo = {
+    nome: 'Kaio', 
+    sexo:'Masculino',
+    peso: 83.4,
+    engordar(p){
+        console.log('engordou')
+        this.peso += p
+    }
+}
+amigo.engordar(2);
+
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`);
