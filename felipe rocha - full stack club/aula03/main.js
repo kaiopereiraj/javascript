e74565d6d9c1f058b987d5e0eb93dfae4e50bc8e
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-11 22:21:53 -0300

    Update main.js

--- felipe rocha - full stack club/aula03/main.js
+++ felipe rocha - full stack club/aula03/main.js
@@ -10,7 +10,13 @@ names.unshift('Ivonete'); // Vai adicionar mais um item no inicio da lista
 
 names.pop(); // Vai remover o último item da lista ...
 
-console.log(names.indexOf('Gil'));
-
-
 console.log(names);
+
+const indexOfGil = names.indexOf('Gil');
+
+const sortedNames = names.sort();
+
+const namesIsArray = Array.isArray(names);
+
+console.log(sortedNames);
+console.log(namesIsArray);
