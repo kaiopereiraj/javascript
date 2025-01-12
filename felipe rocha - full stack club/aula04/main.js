1d0a15e401206e14e1a82d8d98ade1d53d88e969
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-11 22:38:54 -0300

    Update main.js

--- felipe rocha - full stack club/aula04/main.js
+++ felipe rocha - full stack club/aula04/main.js
@@ -1,7 +1,17 @@
 const numbers = [1, 2, 3, 4, 5];
 
 const numbersMultipliedByTwo = numbers.map(function(number) {
-    return number * 2
-})
+   return number * 2
+});
 
-console.log(numbersMultipliedByTwo);
+const ages = [18, 16, 22, 39, 51, 28];
+
+// const evenAges = ages.filter(function (age) {
+//     return age % 2 === 0;
+// });
+
+const sumOfAges = ages.reduce(function(age, accumulator) {
+    return accumulator + age
+}, 0)
+
+console.log(sumOfAges)
