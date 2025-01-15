c5e1013b69f8cadc5261a8b4ed234ca978a314b2
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-14 21:03:24 -0300

    repetição while

---
+++ felipe rocha - full stack club/aula07/loop02.js
@@ -0,0 +1,6 @@
+let idade = 0;
+
+while (idade < 19) {
+    console.log(`${idade} é menor do que 19`)
+    i++
+}
---
+++ felipe rocha - full stack club/aula07/node_3b5f90032c8ae.tmp.js
@@ -0,0 +1,5 @@
+let idade = 0;
+
+while (idade < 19) {
+    console.log(`${idade} é menor do que 19`)
+}
