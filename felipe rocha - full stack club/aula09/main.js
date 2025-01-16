3e56d63ec60e41e40f565aa7263aef8f7e540c20
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-15 21:51:36 -0300

    Create main.js

---
+++ felipe rocha - full stack club/aula09/main.js
@@ -0,0 +1,30 @@
+const x = '';
+// Uma string vazia sempre é false.
+console.log(!!x);
+
+const y = 0;
+// Também é false.
+console.log(!!y);
+
+const a = null;
+// Também é false.
+
+const b = undefined;
+//Também é false.
+
+const list = [];
+console.log(!!list)
+// Uma lista vazia é true.
+
+// caso queira checar se uma lista tem algum valor...
+
+if (list.length > 0){ // para checar se a liste tem algo dentro dela
+
+}
+
+const object = {};
+console.log(!!object);
+// Um objeto vazio também é true.
+
+
+
