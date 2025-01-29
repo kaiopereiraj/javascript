9bc2c1f3a33829537bae63ec920ff2442f025768
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-01-28 22:23:36 -0300

    Update main.js

--- felipe rocha - full stack club/aula012/main.js
+++ felipe rocha - full stack club/aula012/main.js
@@ -1,4 +1,13 @@
-const addUserText = document.querySelector('#add-user');
 
-addUserText.innerText = 'Adicionar Usuário';
+//Selecionam apenas um elemento
+// const addUserText = document.getElementById('add-user')
+//const addUserText = document.querySelector('#add-user');
 
+//addUserText.innerText = 'Adicionar Usuário';
+
+
+//Seleciona multiplos elementos
+const allItems = document.querySelectorAll('.item')
+
+
+allItems[1].innerHTML = 'Mudando o conteúdo do segundo item da lista'
